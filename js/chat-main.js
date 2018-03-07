c1580e24bac0b8e6790c825291a878f656009ed1
edward <h3391831@gmail.com>
2018-03-07 22:45:04 +0800

    Remove autofocus and update style

--- js/chat-main.js
+++ js/chat-main.js
@@ -21,7 +21,7 @@ $(function() {
   var connected = false;
   var typing = false;
   var lastTypingTime;
-  var $currentInput = $usernameInput.focus();
+  //var $currentInput = $usernameInput.focus();
 
   //var socket = io();
   var socket = io.connect('//fe1d4e57.ngrok.io', {reconnect: true});
