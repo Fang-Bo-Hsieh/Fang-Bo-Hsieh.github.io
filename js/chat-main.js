c7e652ebcb3b7bf8a5b3fca36dac8ae040509b9a
edward <h3391831@gmail.com>
2018-03-08 22:03:13 +0800

    Fix input enter error

--- js/chat-main.js
+++ js/chat-main.js
@@ -21,7 +21,7 @@ $(function() {
   var connected = false;
   var typing = false;
   var lastTypingTime;
-  //var $currentInput = $usernameInput.focus();
+  var $currentInput = $usernameInput.focus();
 
   //var socket = io();
   var socket = io.connect('//e8b9b8f8.ngrok.io/', {reconnect: true});
